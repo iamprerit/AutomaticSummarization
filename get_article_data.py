312406e22e3520d1713fbcd84b61367317789fb0
Prerit Agarwal <preritagarwal52@gmail.com>
2015-12-13 14:00:03 +0530

    first commit

---
+++ get_article_data.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+def get_article_links(section_url):
+	BASE_URL="http://timesofindia.indiatimes.com"
+	html=urlopen(section_url).read()
+	soup=BeautifulSoup(html,"lxml")
+	article_grab=soup.find("div","top-story")
+	article_links = [" "+BASE_URL + li.a["href"]+" " for li in article_grab.findAll("li")]
+	f=open("article_links.txt",'w')
+	for x in article_links[0:4]:
+		f.write(x+'\n')
+	f.close()
+	return 
+c=get_article_links("http://timesofindia.indiatimes.com")
+
+
+
+
+
---
+++ get_text.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+def get_article_text():
+	with open("article_links.txt","r") as f:
+		links=f.readlines()
+	i=1;
+	for x in links:
+		real_text(x,i)
+		i=i+1
+	return
+def real_text(site_url,n):
+	html=urlopen(site_url).read()
+	soup=BeautifulSoup(html,'lxml')
+	headline=soup.find("h1","heading1").string
+	text_grab=soup.find("div","section1")
+	f1=open("article_headline"+str(n)+".txt",'w')
+	f2=open("article_text"+str(n)+".txt",'w')
+	f1.write(headline+"\n")
+	f2.write(text_grab.text)
+	f1.close()
+	f2.close()
+	return 
+get_article_text()
+
+
+
+
+
