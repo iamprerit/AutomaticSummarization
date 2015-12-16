fcbd83d475dd6e1001de3c1bc0666be95378cd7c
Prerit Agarwal <preritagarwal52@gmail.com>
2015-12-16 21:26:39 +0530

    eight commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Here are some screenshots of the code in execution:
+![Image 1](https://github.com/iamprerit/AutomaticSummarization/images/ts1.jpg)
+![Image 2](https://github.com/iamprerit/AutomaticSummarization/images/ts2.jpg)
+![Image 3](https://github.com/iamprerit/AutomaticSummarization/images/ts3.jpg)
--- driver.py
+++ driver.py
@@ -6,10 +6,10 @@ def driver():
 	os.system("dialog --backtitle 'by @iamprerit' --title 'Automatic Text Summarisation' --shadow --msgbox 'Press OK to proceed' 7 30")
 	os.system("dialog --backtitle 'by @iamprerit' --title 'Automatic Text Summarisation' --shadow --msgbox 'Top 4 Articles from TOI website would be summarised in 5 sentences' 7 30 ")
 	os.system("dialog --backtitle 'by @iamprerit' --title 'Automatic Text Summarisation' --shadow --nook --msgbox 'Program in progress...please wait(Press OK)' 7 30")
-	get_article_data.get_article_links("http://timesofindia.indiatimes.com")
-	get_text.get_article_text()
-	for x in ['1','2','3','4']:
-		extract_sentences.get_sentences(x,5)
+	# get_article_data.get_article_links("http://timesofindia.indiatimes.com")
+	# get_text.get_article_text()
+	# for x in ['1','2','3','4']:
+	# 	extract_sentences.get_sentences(x,5)
 	os.system("dialog --backtitle 'by @iamprerit' --title 'Automatic Text Summarisation' --shadow --msgbox 'Summarisation Complete. Kindly check the files' 7 30")
 	return
 if __name__ == '__main__':
